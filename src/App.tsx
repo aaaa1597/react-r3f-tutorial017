a77d8a5a3df28b78992cb09f8fae5039a6d8f3fe
jun <jun@Ubt2200.aaaaa>
2023-12-29 11:16:34 +0900

    完成

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, {useRef} from 'react';
 import './App.css';
 import { Canvas, useFrame, MeshProps, useLoader } from '@react-three/fiber'
 import * as THREE from 'three'
-import { Stats, OrbitControls } from '@react-three/drei'
+import { Stats, OrbitControls, Environment } from '@react-three/drei'
 import { useControls } from 'leva'
 
 
@@ -68,6 +68,7 @@ const App = () => {
     <div style={{ width: "75vw", height: "75vh" }}>
       <Canvas camera={{ position: [4, 4, 3] }} shadows>
         <Lights />
+        <Environment preset="forest" background />
         <Box props={{position:[3, 1, 0], name:"meshBasicMaterial",    material: new THREE.MeshBasicMaterial({ map: texture })}}/>
         <Box props={{position:[1, 1, 0], name:"meshNormalMaterial",   material: new THREE.MeshNormalMaterial({flatShading: true,})}}/>
         <Box props={{position:[1, 3, 0], name:"meshPhongMaterial",    material: new THREE.MeshPhongMaterial({flatShading: true, map: texture,})}}/>
